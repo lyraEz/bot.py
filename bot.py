06876aac50dffd1fe780612ccbefc616144ed613
chead-z <155196242+chead-z@users.noreply.github.com>
2024-02-15 04:30:16 -0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix=',', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="Shizukii"))
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello Discord!")
+
+bot.run('Token')
